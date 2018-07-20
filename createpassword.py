ec2a8aaf73527ec4f08a730561016e661953db82
kamranabdullazade <kamran555k@gmail.com>
2018-07-21 00:48:20 +0400

    Update createpassword.py
    Code editing(2)

--- createpassword.py
+++ createpassword.py
@@ -2,26 +2,33 @@ import random
 
 print("***Hello Friends***\nSupport : https://github.com/kamranabdullazade/createpassword\n")
 
-length = int(input("Number of symbols : "))
 
 a = list(range(10))
 
-c = []
 
 character = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z"]
 
 k = ["!","@","#","$","%","/","^","&","*","(",")"]
 
+e = ["*","j"]
 
 
 
-
-b = k + a + character + a + character + k
-
-random.shuffle(b)
+b = k + a + character + a + character + k + e
 
 
-for i in range(0,length):
-    c.append(b[i])
+while True:
+
+  c = []
+  random.shuffle(b)
+
+  length = input("Number of symbols(exit=q) : ")
+  if length.lower() == "q":
+    break
+  
+  length2 = int(length)
+  
+  for i in range(0,length2):
+      c.append(b[i])
         
-print(*c,sep="")
+  print(*c,sep="")
