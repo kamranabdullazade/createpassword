011512b1725f76efff1039c72f5d3bebb23b82c3
kamranabdullazade <kamran555k@gmail.com>
2018-10-06 18:23:38 +0400

    Update createpassword.py

--- createpassword.py
+++ createpassword.py
@@ -1,34 +1,77 @@
+from tkinter import *
 import random
-
-print("***Hello Friends***\nSupport : https://github.com/kamranabdullazade/createpassword\n")
-
-
-a = list(range(10))
-
-
-character = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z"]
-
-k = ["!","@","#","$","%","/","^","&","*","(",")"]
-
-e = ["*","j"]
-
-
-
-b = k + a + character + a + character + k + e
+import ctypes
+import sys
+import string
 
 
 while True:
+    
+    window = Tk()
+    window.geometry("400x200")
 
-  c = []
-  random.shuffle(b)
+    title = window.title("Create Password")
 
-  length = input("Number of symbols(exit=q) : ")
-  if length.lower() == "q":
-    break
-  
-  length2 = int(length)
-  
-  for i in range(0,length2):
-      c.append(b[i])
+    note = Label(text = "***Hello Friends***\nSupport : https://github.com/kamranabdullazade/createpassword\n\nNOTE : You can create passwords up to 90 symbols via the program.\n",fg = "Blue")
+    note.pack()
+
+    article = Label(text = "Password Length : ")
+    article.pack()
+
+    def say():
+    
+
+        a = list(range(10))
+
+
+        character = list(string.ascii_lowercase)
+        character2 = list(string.ascii_uppercase)
+
+        k = ["!","@","#","$","%","/","^","&","*","(",")"]
+
+        e = ["9","5","7","8","2","1"]
+
+
+
+        b = k + a + character2 + a + character + k + e
+
+
+        c = []
+        random.shuffle(b)
+        m1 = int(m.get())
+        if m1 > 100:
+            MessageBox1 = ctypes.windll.user32.MessageBoxW
+            MessageBox1(None, "Please enter a maximum number of 100.", "Warning", 0)
+            
+        for i in range(0,m1):
+            c.append(str(b[i]))
+
+        u = "".join(c)
+        print(u)
+        window.withdraw()
+        window.clipboard_clear()
+        window.clipboard_append(u)
+        window.update() 
+        window.destroy()
+        MessageBox = ctypes.windll.user32.MessageBoxW
+        MessageBox(None, "Copied to Clipboard", "Copied to Clipboard", 0)
         
-  print(*c,sep="")
+
+    def exit():
+        sys.exit()
+        
+        
+    
+
+    m = Entry()
+    m.pack()
+
+
+    button1 = Button(text = "Copy",command=say)
+    button1.pack()
+
+    button2 = Button(text = "Exit",command=exit)
+    button2.pack()
+
+
+    mainloop()
